dc918a93e14bf9423fb78b04266e7ff428b4be0a
João de Paula <joaodepaula1999@gmail.com>
2017-08-14 10:37:21 -0300

    Nova classe principal
    A classe conexão foi matada e inclusa dentro da classe Site que agora conta com o autoload e a conexão...

--- classes/site.class.php
+++ classes/site.class.php
@@ -1,20 +1,51 @@
 <?php
-	#  INCLUSAO DA CONEXAO
-	require_once("classes/conexao.class.php");
-	
+
 	#  CLASSE PRINCIPAL
-	// Header, footer
-	class Site Extends Conexao {
+	class Site {
 
-		//parent::__construct();
+		public function __construct($hef) {
 
-		public function __construct() {
+			session_start();
+
+			/* AUTOLOAD
+			*  metodo mágico que vai carregar os arquivos 
+			*  das classes automaticamente com base no nome 
+			*  da classe. O nome do arquivo php deve ter o
+			*  mesmo nome da classe.
+			*/
+
+			function __autoload($class_name) {
+
+				$class_name = strtolower($class_name);
+				$path = "classes/$class_name.class.php";
+
+				if (file_exists($path)) {
+					require_once($path);
+				} else {
+					die("Arquivo nao encontrado no servidor!");
+				}
+			}
+
 			require_once("include/config.php");
-			require_once("include/header.php");
+
+			if ($hef == true) {
+				require_once("include/header.php");
+			}
+
 		}
 
 		public function __destruct() {
 			require_once("include/footer.php");			
+		}
+
+		public function Conexao() {
+			define('LOCAL', 'localhost');
+			define('BANCO', '99helps');
+			define('USER',  'root');
+			define('PASS',  'admin');
+
+			$conexao = mysql_connect(LOCAL,USER,PASS) or die ("Erro na conexao com o servidor...");
+			$banco   = mysql_select_db(BANCO,$conexao) or die("Erro de selecao de banco...");
 		}
 	}
 
