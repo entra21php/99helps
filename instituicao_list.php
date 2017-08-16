96badb46efedefedcfaa4694e9b64dbefe45a5ea
João de Paula <joaodepaula1999@gmail.com>
2017-08-16 17:55:35 -0300

    Update
    Só falta integrar com a sessão do usuário quando criarmos

--- instituicao_list.php
+++ instituicao_list.php
@@ -7,57 +7,80 @@
 	// <?=$res['resultado_mysql']?_>
 
 	// As verificações em php serão feitas na metódo da classe que chama este arquivo
-	require_once("include/header.php");
-	?>
+	
+	#  A identação está organizada para exibir bonitinho no inspecionar elemento
+	// aqui ela tá feia mesmo...
+?>
+	<section class="container">
 		<p>
 			<ol class="breadcrumb text-center">
 			  <li class="breadcrumb-item"><a href="index.php">Pagina Inicial</a></li>
-			  <li class="breadcrumb-item"><a href="#">Library</a></li>
-			  <li class="breadcrumb-item"><a href="#">Data</a></li>
+			  <li class="breadcrumb-item active">Minhas Instituições</li>
 			</ol>
 		</p>
-	<?php
 
-// LISTAR
-
-	$sql = "SELECT * FROM instituicoes";
-	$consulta = mysql_query($sql) or die(mysql_error());
-	// $rs  = mysql_fetch_array(mysql_query($sql));
-
-echo '<table class="table table-bordered  table-hover text-center">
-		<thead>
-	      <tr>
-	        <th>Nome da ONG</th>
-	        <th>Ver Perfil</th>
-	      </tr>
-	    </thead>';
-	
-
-	$sql = "SELECT nome_fantasia FROM instituicoes";
-
-	
-	while ($rs = mysql_fetch_array($consulta)) {
-		echo "<tr>";
-		echo "<td> " . $rs['nome_fantasia'] . " </td>";
-		echo "<td>";
-	?>
-
-
-	<form method="POST" action="#" name="excluir">
-	<input type="hidden" name="id" value="<?=$rs['id']?>">
-	
-	<button type="submit" class="btn btn-info" name="enviar"> 
-		<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-	</button>
-	</form>
-	
-	<?php
-	echo "</td>";
-	echo "<td>";
-
-
-}
-echo "</table>";
-
-	require_once("include/footer.php")
-?>
+		<div class="card">
+			<h6 class="card-header">Minhas instituições</h6>
+			<div class="card-block">
+				<?php
+					// MELHORAR ESSE SELECT DE ACORDO COM O INSERT DA IMG E DESCRIÇÃO
+					// O WHERE DESSE SELECT SERÁ REFERENTE AO ID DA SESSAO LOGADA NA PLATAFORMA
+					$sql = "SELECT instituicoes.id,instituicoes.nome_fantasia,nivel_acesso FROM usuarios_instituicoes
+							LEFT JOIN instituicoes ON id=fk_instituicao 
+							WHERE fk_usuario = 2 ORDER BY 3 DESC";	
+					$consulta = mysql_query($sql);
+					while ($rs = mysql_fetch_array($consulta)) {
+				?>
+				<div class="card">
+					<div class="card-block">
+						<div class="row">
+							<div class="col-12 col-md-4">
+								<img src="images/evento1.jpg" class="img-fluid img-thumbnail">
+							</div>
+							<div class="col-12 col-md-8">
+								<h4 class="card-title top8"><?=$rs['nome_fantasia']?></h4>
+								<?php 
+									if ($rs['nivel_acesso']=="Membro") {
+										echo '<h6><span class="badge badge-default">Sou Membro</span></h6>';
+									} else {
+										echo '<h6><span class="badge badge-warning">Sou Administrador</span></h6>';
+									}
+								?>
+								<p class="card-text">Aqui ficará o php que pega os 40 primeiros caracteres da descrição da instituição e adicionará...</p>
+								<a href="?ver=<?=$rs['id']?>" class="btn btn-primary top8">Detalhes <i class="fa fa-angle-right" aria-hidden="true"></i></a>	
+							</div>
+						</div>
+					</div>
+				</div><br>
+				<?php 
+					}
+					if (mysql_num_rows($consulta)==0) {
+				?>
+				<div class="card">
+					<div class="card-block">
+						<div class="row">
+							<div class="col-12 col-md-12 text-center">
+								<h4 class="card-title">Você não participa de nenhuma instituição =(</h4>
+								<p class="card-text">Parece que você não tem muita intimidade com nossa plataforma ainda. Procure por eventos e solicite para participar das instituições que você desejaria ser membro, ou então veja como criar uma instituição abaixo.</p>
+								<a href="?add" class="btn btn-secondary top8">Procurar eventos <i class="fa fa-angle-right" aria-hidden="true"></i></a>	
+							</div>
+						</div>
+					</div>
+				</div><br>
+				<?php 
+					}
+				?>
+				<div class="card">
+					<div class="card-block">
+						<div class="row">
+							<div class="col-12 col-md-12 text-center">
+								<h4 class="card-title">Cadastre sua instituição</h4>
+								<p class="card-text">Se você é representante legal de alguma instituição, fundação, entidade ou organização não governamental você pode se cadastrar em nossa plataforma para divulgar seus eventos e ações, atraindo novos colaboradores e membros para seus projetos. <mark> Cadastro requer o envio de documentos oficiais que comprovem a real existencia da instituição e está sujeito a aprovação no sistema. </mark></p>
+								<a href="?add" class="btn btn-success top8">Cadastrar minha instituição <i class="fa fa-angle-right" aria-hidden="true"></i></a>	
+							</div>
+						</div>
+					</div>
+				</div><br>
+			</div>
+		</div><br>
+	</section>
