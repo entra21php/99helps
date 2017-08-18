1438bada1f05844d12cf9657193c8b08015ec651
João de Paula <joaodepaula1999@gmail.com>
2017-08-18 02:09:34 -0300

    Update

--- include/footer.php
+++ include/footer.php
@@ -1,31 +1,22 @@
 		<footer class="row footer bg-primary text-white">
-				<div class="container">
-						<div class="row">
-							<div class="col-xs-2 col-sm-12 col-md-12 col-lg-12 sitemap text-center roda">
-								<small>
-									|<a href="#" class="btn btn-social-icon btn-twitter text-white">
-    								<span class="fa fa-twitter"></span>
-  									</a>|
-  									<a href="#" class="btn btn-social-icon btn-facebook text-white">
-    								<span class="fa fa-facebook"></span>
-  									</a>|
-  									<a href="#" class="btn btn-social-icon btn-instagram text-white">
-    								<span class="fa fa-instagram"></span>
-  									</a>|
-									<a href="#" class="btn btn-social-icon btn-google text-white">
-    								<span class="fa fa-google"></span>
-  									</a>|
-  									<a href="#" class="btn btn-social-icon btn-linkedin text-white">
-    								<span class="fa fa-linkedin"></span>
-  									</a>|
-								</small>
-							</div>
-						</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2 col-sm-12 col-md-12 col-lg-12 sitemap text-center roda">
+						<small>
+							<a href="#" class="btn btn-social-icon btn-twitter text-white social"><span class="fa fa-twitter"></span></a>
+  							<a href="#" class="btn btn-social-icon btn-facebook text-white social"><span class="fa fa-facebook"></span></a>
+  							<a href="#" class="btn btn-social-icon btn-instagram text-white social"><span class="fa fa-instagram"></span></a>
+							<a href="#" class="btn btn-social-icon btn-google text-white social"><span class="fa fa-google"></span></a>
+  							<a href="#" class="btn btn-social-icon btn-linkedin text-white social"><span class="fa fa-linkedin"></span></a>
+						</small>
 					</div>
+				</div>
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 sitemap">
-			<hr>
-		<p class="text-center"><b>99helps © Todos os direitos reservados.</b></p>			
-		</div>
+					<hr>
+					<p class="text-center">99helps © Todos os direitos reservados.</p>
+				</div>	
+			</div>
+		</footer>
 	
 	<!-- JQUERY-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -33,3 +24,5 @@
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
     <!-- BOOTSTRAP 4 ALPHA 6-->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
