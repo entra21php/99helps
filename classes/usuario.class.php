202e3d8526821f50796e6898100e86c2e88d1387
Jefferson030 <30695021+Jefferson030@users.noreply.github.com>
2017-08-21 11:05:15 -0300

    Update usuario.class.php

--- classes/usuario.class.php
+++ classes/usuario.class.php
@@ -3,17 +3,18 @@
 	// Desenvolvido por: Douglas e Jefferson
 class Usuario Extends Site {
 
+	public $id 			= "";
 	public $nome 			= "";
 	public $sobrenome 		= "";
 	public $sexo 			= "";
 	public $datanascimento 	= "";
-	public $imagem_perfil 		= "";
+	public $imagem_perfil 	= "";
 	public $estado 			= "";
 	public $fk_cidades 		= "";
 	public $email 			= "";
-	public $email_confere		="";
+	public $email_confere	= "";
 	public $senha 			= "";
-	public $descricao		="";
+	public $descricao		= "";
 	public $ativo			= "";
 	public $erro 			=false;
 
@@ -22,13 +23,27 @@ class Usuario Extends Site {
 		# Verifica a acao do momento (perfil_usuario, del, edt, add)
 		if (isset($_GET['edit'])) {
 
-			// se é edição
-			$this->editCadastro();
+			// se esta salvando edição
+			if (isset($_POST['enviar'])) {
+
+				$this->editCadastro();
+
+			// senao, esta exibindo form com os dados
+			} else {
+
+				$this->id=$_GET['edit'];
+				// se é edição
+				// $this->editCadastro($this->id);
+				 $this->verCadastro($this->id);
+
+				$this->formCadastro($this->id);
+			 }
 
 		} elseif (isset($_GET['editCadastro'])) {
 
 			// se é cadastro
 			$this->insertForm('edit',$_GET['editCadastro']);
+
 
 		} elseif (isset($_GET['add'])) {
 
@@ -41,7 +56,12 @@ class Usuario Extends Site {
 			// se é cadastro
 			$this->insertForm('add',0);
 
-		} elseif (isset($_GET['del'])) {
+		} elseif ((isset($_GET['ver'])) && (isset($_GET['acao'])) ) {
+
+			// VER usuario
+			$this->verCadastro($_GET['ver']);
+
+		}elseif (isset($_GET['del'])) {
 
 			// se é delete
 			$this->delCadastro($_GET['del']);
@@ -64,14 +84,13 @@ class Usuario Extends Site {
 			$this->nome 			=$_POST['nome'];
 			$this->sobrenome 		=$_POST['sobrenome'];
 			$this->sexo 			=$_POST['sexo'];
-			$this->datanascimento 	    	=$_POST['datanascimento'];
-			
-			$this->estado 		    	=$_POST['estado'];
+			$this->datanascimento 	=$_POST['datanascimento'];
+			$this->estado 		    =$_POST['estado'];
 			$this->fk_cidades 		=$_POST['fk_cidades'];
 			$this->email 			=$_POST['email'];
-			$this->email_confere		=$_POST['email_confere']; 	
-			$this->senha 		    	=$_POST['senha'];
-			$this->senha_confere	   	=$_POST['senha_confere'];
+			$this->email_confere	=$_POST['email_confere']; 	
+			$this->senha 		    =$_POST['senha'];
+			$this->senha_confere	=$_POST['senha_confere'];
 			$this->interesses		=$_POST['interesses'];
 			$this->descricao		=$_POST['descricao'];
 			$this->exibe_form 		    = true;
@@ -204,46 +223,85 @@ public function delCadastro() {
 	}
 }
 
-public function edtCadastro() {
-		# Recebe informações do form da pagina e realiza edt
-	$this->nome 			= $_POST['nome'];
-	$this->sobrenome 		= $_POST['sobrenome'];
-	$this->sexo 			= $_POST['sexo'];
-	$this->datanascimento 		= $_POST['datanascimento'];
-	$this->imagem_perfil 		= $_POST ['imagem_perfil'];
-	$this->estado 			= $_POST['estado'];
-	$this->fk_cidades 		=$_POST['fk_cidades'];
-	$this->email 			=$_POST['email'];
-	$this->senha 			=$_POST['senha'];
-	$this->ativo			=$_POST['ativo'];
+public function editCadastro() {
 
-		//atualizar no banco de dados
-	$sql = "UPDATE usuarios_fisico SET  	nome 			='$nome',
-	sobrenome		='$this->sobrenome',
-	sexo 			='$this->sexo',
-	datanascimento 	='$this->datanascimento',
-	imagem_perfil		='$this->imagem_perfil',*/
-	estado 			='$this->estado',
-	fk_cidades 		='$this->fk_cidades',
-	email 			='$this->email',
-	senha			='$this->senha',
-	ativo			='$this->ativo'
-	WHERE id 		=" . $this->id;
+		
+		# Recebe informações do conteudo da pagina e realiza insert
+		if(isset($_POST['enviar'])) {
 
-	if (mysql_query($sql)) {
-		echo '<p>Editado com sucesso</p>';
-	} else {
-		echo '<p>Problemas na edição!</p>';
-	}
+
+			$this->id 				= $_POST['id'];
+			$this->nome 			= $_POST['nome'];
+			$this->sobrenome 		= $_POST['sobrenome'];
+			$this->sexo 			= $_POST['sexo'];
+			$this->datanascimento 	= $_POST['datanascimento'];
+			$this->estado 			= $_POST['estado'];
+			$this->fk_cidades 		= $_POST['fk_cidades'];	
+			$this->descricao 		= $_POST['descricao'];
+
+
+
+			
+
+
+			#VERIFICACAO DE ERROS
+
+			// Verifica se existe campo vazio
+		if ((empty($this->nome))  ||  (empty($this->sobrenome)) || (empty($this->sexo)) || (empty($this->datanascimento)) || (empty($this->interesses)) || (empty($this->estado))  || (empty($this->fk_cidades)) ) {
+			$erro = true;
+			echo '<div class="alert alert-danger" role="alert">Preencha todos os campos </div>';
+		}
+			
+
+			// Exibe erro se ele existir
+		if ($this->erro == false) {
+
+						$sql = "UPDATE usuarios_fisico SET  	
+			nome 			='$this->nome',
+			sobrenome		='$this->sobrenome',
+			sexo 			='$this->sexo',
+			datanascimento 	='$this->datanascimento',
+			estado 			='$this->estado',
+			fk_cidades 		='$this->fk_cidades',
+			descricao 		='$this->descricao'
+			WHERE id 		=" . $this->id;
+			if (mysql_query($sql)) {
+				echo '<p>Editado com sucesso</p>';
+			} else {
+				echo '<p>Problemas na edição!</p>';
+			}
+		}
+
 }
+}
+public function verCadastro($id) {
+		// Consulta
 
-public function verCadastro() {
+			$sql = "SELECT * FROM usuarios_fisico WHERE id=".$id;
+			$consulta = mysql_query($sql);
+			$rsvar = mysql_fetch_array($consulta);
+
+			$this->nome 			= $rsvar['nome'];
+			$this->sobrenome 		= $rsvar['sobrenome'];
+			$this->sexo 			= $rsvar['sexo'];
+			$this->datanascimento 	= $rsvar['datanascimento'];
+			$this->imagem_perfil 	= $rsvar['imagem_perfil'];
+			$this->estado 			= $rsvar['estado'];
+			$this->fk_cidades 		= $rsvar['fk_cidades'];
+			$this->descricao 		= $rsvar['descricao'];
+			$this->ativo			= $rsvar['ativo'];
+
 			#  Select do usuario e require do html da página (perfil_usuario -> pagina do perfil detalhado)
 			// Aqui será a página bonita que exibe o perfil do usuário de acordo com o parametro id
-			require_once("perfil_usuario.php");
-}
+		
+					require_once("usuario_perfil.php");
+			
+			}
+			
+			
+	
 
-public function formCadastro() {
+public function formCadastro($id = 0) {
 			# Select dos dados e require do html da página (form_usuario -> pagina do perfil detalhado)
 			// Aqui os inputs virão preenchidos com as infos do perfil de acordo com o select por id
 
