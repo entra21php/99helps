64ddb175281ccdad3fd133107d6e63f7a2b56084
João de Paula <joaodepaula1999@gmail.com>
2017-08-30 11:50:21 -0300

    Update Char.JS

--- classes/footer.php
+++ classes/footer.php
@@ -19,10 +19,12 @@
 		</footer>
 	
 	<!-- JQUERY-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- BOOTSTRAP 4 OFICIAL-->
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
-    <!-- BOOTSTRAP 4 ALPHA 6-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- BOOTSTRAP 4 OFICIAL-->
+	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
+	<!-- BOOTSTRAP 4 ALPHA 6-->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<!-- CHAR.JS -->
+	<script src="js/Chart.min.js"></script>
 </body>
 </html>
