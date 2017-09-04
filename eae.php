394b7626e8d1aa85351f72fb24f4b888adb9ad7c
João de Paula <joaodepaula1999@gmail.com>
2017-09-04 09:02:03 -0300

    Teste de header ligadi

---
+++ eae.php
@@ -0,0 +1,30 @@
+<?php
+	require_once("classes/site.class.php");
+	$site = new Site($hef=true);
+
+?>
+
+	<nav class="row bg-primary menu">
+		<div class="container">
+			<div class="col-9">
+				<ul>
+					<a href=""><li class="menu-logo">99helps</li></a>
+					<a href=""><li>Eventos</li></a>
+				</ul>
+			</div>
+			<div class="col-1 foto">
+				<ul>
+					<img src="images/foto_padrao.png" class="">
+				</ul>
+			</div>
+		</div>
+	</nav>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
