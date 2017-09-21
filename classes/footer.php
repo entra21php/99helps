c10c25d3c1533ec486eaad6b435d0fdd9608673f
Julia Goedert <30660247+goejulia@users.noreply.github.com>
2017-09-21 11:35:40 -0300

    Update footer.php

--- classes/footer.php
+++ classes/footer.php
@@ -3,11 +3,9 @@
 				<div class="row">
 					<div class="col-xs-2 col-sm-12 col-md-12 col-lg-12 sitemap text-center roda">
 						<small>
-							<a href="#" class="btn btn-social-icon btn-twitter text-white social"><span class="fa fa-twitter"></span></a>
-  							<a href="https://www.facebook.com/99Helps/" class="btn btn-social-icon btn-facebook text-white social"><span class="fa fa-facebook"></span></a>
-  							<a href="#" class="btn btn-social-icon btn-instagram text-white social"><span class="fa fa-instagram"></span></a>
-							<a href="#" class="btn btn-social-icon btn-google text-white social"><span class="fa fa-google"></span></a>
-  							<a href="#" class="btn btn-social-icon btn-linkedin text-white social"><span class="fa fa-linkedin"></span></a>
+							<a href="https://twitter.com/Helps99" class="btn btn-social-icon btn-twitter text-white social"><span class="fa fa-twitter"></span></a>
+							<a href="https://www.facebook.com/99Helps/" class="btn btn-social-icon btn-facebook text-white social"><span class="fa fa-facebook"></span></a>
+							<a href="https://twitter.com/99Helps" class="btn btn-social-icon btn-instagram text-white social"><span class="fa fa-instagram"></span></a>
 						</small>
 					</div>
 				</div>
@@ -17,14 +15,14 @@
 				</div>	
 			</div>
 		</footer>
-	
-	<!-- JQUERY-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<!-- BOOTSTRAP 4 OFICIAL-->
-	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
-	<!-- BOOTSTRAP 4 ALPHA 6-->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-	<!-- CHAR.JS -->
-	<script src="js/Chart.min.js"></script>
-</body>
-</html>
+
+		<!-- JQUERY-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- BOOTSTRAP 4 OFICIAL-->
+		<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> -->
+		<!-- BOOTSTRAP 4 ALPHA 6-->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<!-- CHAR.JS -->
+		<script src="js/Chart.min.js"></script>
+	</body>
+	</html>
